6efbdeee59d2e519a7708692aaec29ec95c12a91
Brennon Loveless <brennon.loveless@gmail.com>
2015-10-10 20:52:26 -0600

    Streamline and reduce as much as I can. BTW the code is 349 lines long right now ;)

--- NRF24L01.ino
+++ NRF24L01.ino
@@ -70,9 +70,6 @@ void nrfInit()
   //pipe 0-5, bytes 1-32
   nrfSetRXPayload(0, 3);
 
-  //Get Reg7 Status, 1=print screen
-  nrfGetAddress(7, 1);
-
   //register#, bit#, and value 0 or 1, ::  0,0,1 RX Mode
   nrfSetAddressBit(0, 0);
 
@@ -151,7 +148,8 @@ void nrfGetData()
 {
   int i;
   digitalWrite(CSN_pin, LOW);
-  data_in[0] = SPI.transfer(B01100001);//read the payload
+  //read the payload
+  data_in[0] = SPI.transfer(B01100001);
   data_in[1] = SPI.transfer(B00000000);
   data_in[2] = SPI.transfer(B00000000);
   data_in[3] = SPI.transfer(B00000000);
@@ -270,23 +268,18 @@ void nrfClearAddressBit(byte address, byte bit_address)
 void nrfWriteAddressBit(byte address, byte bit_address, byte val)
 {
   //first read out the register
-  nrfGetAddress(address, 0);
+  byte registerByte = nrfGetAddress(address);
 
   //if we want to write a one to the bit then set the bit in the register we read
   if(val==1) {
-    bitSet(data_in[1], bit_address);
+    bitSet(registerByte, bit_address);
   } else {
     //clear it if not
-    bitClear(data_in[1], bit_address);
+    bitClear(registerByte, bit_address);
   }
 
-  //now we'll write the modified data back in
-  digitalWrite(CSN_pin, LOW);
-  //a write to a register adds 32
-  data_in[0] = SPI.transfer(32+address);
-  //write the modified register
-  data_in[1] = SPI.transfer(data_in[1]);
-  digitalWrite(CSN_pin, HIGH);
+  // Write the byte to the module
+  nrfWriteByte(32+address, registerByte);
 }
 
 //there are three interrupt flags in the NRF.  This routine resets them all no matter what
@@ -329,261 +322,28 @@ void nrfPing()
 
 //     MASSIVE ROUTINE HERE - PUT ALL ROUTINES ABOVE THIS
 
-void nrfGetAddress(byte address, byte info)
+byte nrfGetAddress(byte address)
 {
   //send the address and either a 1 or 0 if you want to do a serial print of the address
   //after a call to this routine, data_in[1] will equal the address you called
 
+  return nrfWriteByte(address, B00000000);
+}
+
+byte nrfWriteByte(byte address, byte data)
+{
+  byte returnByte;
+  
+  //now we'll write the modified data back in
   digitalWrite(CSN_pin, LOW);
-  data_in[0] = SPI.transfer(address);
-  data_in[1] = SPI.transfer(B00000000);
+  //a write to a register adds 32
+  returnByte = SPI.transfer(address);
+  //write the modified register
+  returnByte = SPI.transfer(data);
   digitalWrite(CSN_pin, HIGH);
 
-  // if the user wanted it, you will get a print out of the register - good fo debugging
-  if(info==1){
-    Serial.print("R");
-    Serial.print(address);
-    
-    switch (address) {
-      case 0:
-        Serial.print(" CONFIG REGISTER =");
-        Serial.println(data_in[1]);
-        Serial.print("PRIM_RX = ");
-   
-        if(bitRead(data_in[1],0)) {
-          Serial.println("PRX");
-        } else {
-          Serial.println("PTX");
-        }
-   
-        Serial.print("PWR_UP = ");
-        if(bitRead(data_in[1],1)) {
-          Serial.println("POWER UP");
-        } else {
-          Serial.println("POWER DOWN");
-        }
-   
-        Serial.print("CRCO = ");
-        if(bitRead(data_in[1],2)) {
-          Serial.println("2Bytes");
-        } else {
-          Serial.println("1Byte");
-        }
-   
-        Serial.print("EN_CRC = ");
-        if(bitRead(data_in[1],3)) {
-          Serial.println("Enabled");
-        } else {
-          Serial.println("Disabled");
-        }
- 
-        Serial.print("MASK_MAX_RT = ");
-        if(bitRead(data_in[1],4)) {
-          Serial.println("Interrupt not reflected on the IRQ pin");
-        } else {
-          Serial.println("Reflect MAX_RT as active low interrupt on the IRQ pin");
-        }
- 
-        Serial.print("MASK_TX_DS = ");
-        if(bitRead(data_in[1],5)) {
-          Serial.println("Interrupt not reflected on the IRQ pin");
-        } else {
-          Serial.println("Reflect TX_DS as active low interrupt on the IRQ pin");
-        }
-   
-        Serial.print("MASK_RX_DR = ");
-        if(bitRead(data_in[1],6)) {
-          Serial.println("Interrupt not reflected on the IRQ pin");
-        } else {
-          Serial.println("Reflect RX_DR as active low interrupt on the IRQ pin");
-        }
-        
-        break; // 0
-        
-      case 1:
-        Serial.print(" EN_AA REGISTER Enhanced ShockBurst =");
-        Serial.println(data_in[1]);
-        break; // 1
-        
-      case 2:
-        Serial.print(" EN_RXADDR REGISTER Enabled RX Addresses =");
-        Serial.println(data_in[1]);
-        break; // 2
-        
-      case 3:
-        Serial.print(" SETUP_AW REGISTER Setup of Address Widths =");
-        Serial.println(data_in[1]);
-        break; // 3
-        
-      case 4:
-        Serial.print(" SETUP_RETR REGISTER Setup of Automatic Retransmission =");
-        Serial.println(data_in[1]);
-        break; // 4
-        
-      case 5:
-        Serial.print(" RF_CH REGISTER RF Channel =");
-        Serial.println(data_in[1]);
-        break; // 5
-        
-      case 6:
-        Serial.print(" RF_SETUP REGISTER RF Setup Register =");
-        Serial.println(data_in[1]);
-        Serial.print("RF Power = ");
-        Serial.print(bitRead(data_in[1],2));
-        Serial.println(bitRead(data_in[1],1));
-        Serial.print("RF_DR_HIGH = ");
-        Serial.println(bitRead(data_in[1],3));
-        Serial.print("PLL_LOCK = ");
-        Serial.println(bitRead(data_in[1],4));
-        Serial.print("RF_DR_LOW = ");
-        Serial.println(bitRead(data_in[1],5));  
-        Serial.print("CONT_WAVE = ");
-        Serial.println(bitRead(data_in[1],7));   
-        break; // 6
-        
-      case 7:
-        Serial.print(" STATUS REGISTER  =");
-        Serial.println(data_in[1]);
-        
-        Serial.print("TX_FULL = ");
-        if(bitRead(data_in[1],0)) {
-          Serial.println("TX FIFO full");
-        } else {
-          Serial.println("TX FIFO Not full");
-        }
-   
-        Serial.print("RX_P_NO = ");
-        if(bitRead(data_in[1],1)&&(data_in[1],2)&&(data_in[1],3)) {
-          Serial.println("RX FIFO Empty");
-        } else {
-          Serial.println(bitRead(data_in[1],1)+(bitRead(data_in[1],2)<<1)+(bitRead(data_in[1],2)<<2));
-        }
-        
-        Serial.print("MAX_RT Interrupt = ");
-        Serial.println(bitRead(data_in[1],4));
-        Serial.print("TX_DS Interrupt = ");
-        Serial.println(bitRead(data_in[1],5));
-        Serial.print("RX_DR Interrupt = ");
-        Serial.println(bitRead(data_in[1],6));
-        break; // 7
-        
-      case 8:
-        Serial.print(" OBSERVE_TX REGISTER Transmit observe register  =");
-        Serial.println(data_in[1]);
-        Serial.print("ARC_CNT = "); 
-        Serial.println(bitRead(data_in[1],0)+(bitRead(data_in[1],1)<<1)+(bitRead(data_in[1],2)<<2)+(bitRead(data_in[1],3)<<3));
-        Serial.print("PLOS_CNT = "); 
-        Serial.println(bitRead(data_in[1],4)+(bitRead(data_in[1],5)<<1)+(bitRead(data_in[1],6)<<2)+(bitRead(data_in[1],7)<<3));
-        break; // 8
-        
-      case 9:
-        Serial.print(" RPD REGISTER Received Power Detector =");
-        Serial.println(bitRead(data_in[1],0));
-        break; // 9
-        
-      case 10:
-        Serial.print(" RX_ADDR_P0 LSB =");
-        Serial.println(data_in[1]);
-        break; // 10
-        
-      case 11:
-        Serial.print(" RX_ADDR_P1 LSB =");
-        Serial.println(data_in[1]);
-        break; // 11
-        
-      case 12:
-        Serial.print(" RX_ADDR_P2 LSB =");
-        Serial.println(data_in[1]);
-        break; // 12
-        
-      case 13:
-        Serial.print(" RX_ADDR_P3 LSB =");
-        Serial.println(data_in[1]);
-        break; // 13
-        
-      case 14:
-        Serial.print(" RX_ADDR_P4 LSB =");
-        Serial.println(data_in[1]);
-        break; // 14
-        
-      case 15:
-        Serial.print(" RX_ADDR_P5 LSB =");
-        Serial.println(data_in[1]);
-        break; // 15
-
-      case 16:
-        Serial.print(" TX_ADDR LSB =");
-        Serial.println(data_in[1]);
-        break; // 16
-        
-      case 17:
-        Serial.print(" RX_PW_P0 RX payload =");
-        Serial.println(data_in[1]);
-        break; // 17
-        
-      case 18:
-        Serial.print(" RX_PW_P1 RX payload =");
-        Serial.println(data_in[1]);
-        break; // 18
-        
-      case 19:
-        Serial.print(" RX_PW_P2 RX payload =");
-        Serial.println(data_in[1]);
-        break; // 19
-        
-      case 20:
-        Serial.print(" RX_PW_P3 RX payload =");
-        Serial.println(data_in[1]);
-        break; // 20
-        
-      case 21:
-        Serial.print(" RX_PW_P4 RX payload =");
-        Serial.println(data_in[1]);
-        break; // 21
-
-      case 22:
-        Serial.print(" RX_PW_P5 RX payload =");
-        Serial.println(data_in[1]);
-        break; // 22
-
-      case 23:
-        Serial.print(" FIFO_STATUS Register =");
-        Serial.println(data_in[1]);
-        
-        Serial.print("RX_EMPTY = ");
-        if(bitRead(data_in[1],0)) {
-          Serial.println("RX FIFO empty");
-        } else {
-          Serial.println("Data in RX FIFO");
-        }
-   
-        Serial.print("RX_EMPTY = ");
-        if(bitRead(data_in[1],1)) {
-          Serial.println("RX FIFO full");
-        } else {
-          Serial.println("Available locations in RX FIFO");
-        }
- 
-        Serial.print("TX_EMPTY = ");
-        if(bitRead(data_in[1],4)) {
-          Serial.println("TX FIFO empty");
-        } else {
-          Serial.println("Data in TX FIFO");
-        }
-  
-        Serial.print("TX_FULL = ");
-        if(bitRead(data_in[1],5)) {
-          Serial.println("TX FIFO full");
-        } else {
-          Serial.println("Available locations in TX FIFO");
-        }
-        
-        Serial.print("TX_REUSE = ");
-        Serial.println(bitRead(data_in[1],6));
-        break; // 23
-        
-    } // switch
-  } // if 1
+  return returnByte;
 }
+
 
 
